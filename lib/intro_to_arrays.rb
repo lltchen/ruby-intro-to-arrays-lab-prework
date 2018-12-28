30abc41c5f832c8a707d49c0adc3ee6ca1b5c471
lltchen <lltchen1@gmail.com>
2018-12-28 12:54:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,27 +10,27 @@
   def first_element(array)
     array[0]
   end
-  
+
   def third_element(array)
     array[2]
-    
+
   end
 
   def last_element(array)
     array[-1]
-    
+
   end
 
   def first_element_with_array_methods(array)
     array.first
   end
-  
+
   def last_element_with_array_methods(array)
     array.last
-  
+
   end
 
   def length_of_array(array)
     array.count
-    
+
   end
